723f84526081900e8838491583c1f35bc2c841c2
ErnestoC <38075152+ErnestoCb@users.noreply.github.com>
2018-12-13 18:37:13 -0300

    Inicial Commit

---
+++ EtiquetasBlumar/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using iTextSharp;
+using iTextSharp.text.pdf;
+
+namespace EtiquetasBlumar
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Barcode128 code128 = new Barcode128();
+        Random random = new Random();
+        int randomNumber = -1;
+        string nf;
+
+
+        private MainWindow instance;
+        public MainWindow()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+    }
+}
